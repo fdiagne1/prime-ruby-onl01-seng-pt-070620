06d7151e74b84c221596e5bbcb3f68fca7f05547
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-17 18:54:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,14 @@
-def prime_numbers(integer)
-  i = 2
+# def prime_numbers(integer)
+#   i = 2
   
-  while i < integer
-    return false if integer % i == 0
-    i += 1
-end
-    true
-end
+#   while i < integer
+#     return false if integer % i == 0
+#     i += 1
+# end
+#     true
+# end
 
 
+def prime? i
+  (2..Math.sqrt(i)).none? {|f| i % f == 0}
+end
