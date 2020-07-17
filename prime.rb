c97a016cd3897bb8a80a49d01ef4bb8946c2b936
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-17 18:55:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -9,6 +9,12 @@
 # end
 
 
-def prime? i
-  (2..Math.sqrt(i)).none? {|f| i % f == 0}
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
